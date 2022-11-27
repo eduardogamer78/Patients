b14f1ef4c943742e44aa51398275709a2b3445fa
“eduardogamer78” <cs-royalbrasil2013@hotmail.com>
2022-11-27 19:35:24 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <q-layout view="lHh Lpr lFf">
+    <SideBar />
+    <RightSidebar />
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+import RightSidebar from "@/components/RightSidebar";
+import SideBar from "@/components/SideBar";
+export default {
+  name: 'LayoutDefault',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    SideBar,
+    RightSidebar,
+  },
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  setup () {
+    return {
+
+    }
+  }
 }
-</style>
+</script>
